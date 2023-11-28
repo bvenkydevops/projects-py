fd1a9079aadb984e76946fc558a106ee82f92bd1
bvenkydevops <bvenkydevops@gmail.com>
2023-11-28 09:13:26 +0530

    1st commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    test_suite='tests',
+)
